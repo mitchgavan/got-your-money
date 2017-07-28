661836fadff23cfb2b1e73434cfa79c894b482df
Mitch Gavan <mitchgavan@gmail.com>
2017-07-28 22:59:50 +1000

    added styled-components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
+import styled, { keyframes } from 'styled-components';
 import logo from './logo.svg';
-import './App.css';
+
+const rotate360 = keyframes`
+  from { transform: rotate(0deg); }
+  to { transform: rotate(360deg); }
+`;
+
+const Header = styled.div`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`;
+
+const Logo = styled.img`
+  animation: ${rotate360} infinite 20s linear;
+  height: 80px;
+`;
+
+const Intro = styled.div`
+  padding: 20px;
+  font-size: large;
+`;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <Header>
+          <Logo src={logo} alt="logo" />
           <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
+        </Header>
+        <Intro>
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </Intro>
       </div>
     );
   }
