a612b53558ac1b0343f971b694ed1fe0ae30db05
Mitch Gavan <mitchgavan@gmail.com>
2017-08-27 12:13:27 +1000

    changed reducer from switch statement to take advantage of createReducer

--- src/reducers/items.js
+++ src/reducers/items.js
@@ -3,26 +3,30 @@ import {
   reject,
   equals,
 } from 'ramda'
+import createReducer from '../utilities/createReducer'
 
-// const initialState = {
-//   expenseItems: []
+const initialState = []
+
+const addItem = (state, action) => prepend({
+  id: action.id,
+  title: action.title,
+  cost: action.cost
+}, state)
+
+const removeItem = (state, action) =>
+  reject(item => equals(item.id, action.id), state)
+
+// const actionHandlers = {
+//   ADD_ITEM: (state, action) => prepend({
+//     id: action.id,
+//     title: action.title,
+//     cost: action.cost
+//   }, state),
+//   REMOVE_ITEM: (state, action) =>
+//     reject(item => equals(item.id, action.id), state)
 // }
 
-export default (state = [], action) => {
-
-  const getItem = item => equals(item.id, action.id)
-
-  switch (action.type) {
-    case 'ADD_ITEM':
-      return prepend({
-        id: action.id,
-        title: action.title,
-        cost: action.cost
-      }, state)
-    case 'REMOVE_ITEM':
-      return reject(getItem, state)
-
-    default:
-      return state
-  }
-}
+export default createReducer(initialState, {
+  ADD_ITEM: addItem,
+  REMOVE_ITEM: removeItem
+});
