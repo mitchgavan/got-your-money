cc4a15cdac0c3da619e41e24809f2308a2bd4d73
Mitch Gavan <mitchgavan@gmail.com>
2018-07-29 11:41:32 +1000

    wire-in basic aws mobilehub settings

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'react-router-redux'
-
+import Amplify from 'aws-amplify'
+import configuration from './aws-exports'
 import store, { history } from './store'
 import App from './App'
 import { unregister } from './registerServiceWorker'
 import 'normalize.css'
 import './theme/index.css'
+
+Amplify.configure(configuration)
 
 render(
   <Provider store={store}>
