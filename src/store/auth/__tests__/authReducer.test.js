de4ea951f4c13f81a98a9f4b7708fb44b1de99d1
Mitch Gavan <mitchgavan@gmail.com>
2018-10-02 07:54:52 +1000

    test: auth store tests

---
+++ src/store/auth/__tests__/authReducer.test.js
@@ -0,0 +1,38 @@
+import authReducer from '../authReducer'
+import { SIGN_IN_SUCCESS, CURRENT_SESSION_SUCCESS } from '../authActions'
+
+describe('authReducer', () => {
+  describe('signInSuccess', () => {
+    test('it should set isAuthenticated to true', () => {
+      const state = {
+        isAuthenticated: false,
+      }
+      const action = {
+        type: SIGN_IN_SUCCESS,
+        payload: {},
+      }
+      const newState = {
+        isAuthenticated: true,
+      }
+
+      expect(authReducer(state, action)).toEqual(newState)
+    })
+  })
+
+  describe('getCurrentSessionSuccess', () => {
+    test('it should set isAuthenticated to true', () => {
+      const state = {
+        isAuthenticated: false,
+      }
+      const action = {
+        type: CURRENT_SESSION_SUCCESS,
+        payload: {},
+      }
+      const newState = {
+        isAuthenticated: true,
+      }
+
+      expect(authReducer(state, action)).toEqual(newState)
+    })
+  })
+})
